dba85df64059100bc22f96ad54597f22d4026dec
antoniogiachin <giachinantonio@gmail.com>
2022-02-25 15:20:48 +0100

    impostazione iniziale main header

---
+++ src/components/myHeader.vue
@@ -0,0 +1,36 @@
+<template>
+
+    <header class="bg-black">
+
+        <div class="container-fluid">
+        <div class="row">
+
+            <div class="col text-uppercase">
+                <span class="text-danger fs-4">Boolfix</span> 
+            </div>
+
+            <div class="col d-flex justify-content-end">
+                <input type="text" placeholder="Cerca qui">
+                <button>Cerca</button>
+            </div>
+
+        </div>
+        </div>
+
+    </header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    height: 80px;
+    display: flex;
+    align-items: center;
+}
+</style>
---
+++ src/components/myMain.vue
@@ -0,0 +1,28 @@
+<template>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    scheda film
+                    <ul>
+                        <li>a</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+main{
+    background-color: #434343;
+    height: calc(100vh - 80px);
+    width: 100%;
+}
+</style>
